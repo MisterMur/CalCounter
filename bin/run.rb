c22f789289041d7db638425b4d71c0bff7cb80fe
mistermur <brian.j.murillo@gmail.com>
2019-01-22 16:39:25 -0500

    response from api works

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,20 @@
 require_relative '../config/environment'
+require 'rest-client'
+require 'json'
 
+require 'pry'
+def get_food_from_api(food)
+  #make the web request
+  # puts 'in get character'
+  apikey = "https://api.nal.usda.gov/ndb/search/?format=json&q=cookie&sort=n&max=25&offset=0&api_key=m7tJlPDeol0BRpU94StlarX7J2owCr33rxxJS8mP"
+  response_string = RestClient.get(apikey)
+  response_hash = JSON.parse(response_string)
+  binding.pry
+  response_hash.fetch('foods').each do |f|
+    puts f["food"]["desc"]
+  end
+  # binding.pry
+end
 
-
+get_food_from_api('Cheese, cheddar')
 puts "HELLO WORLD"
