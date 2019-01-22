cc2e84a4cea647b58d167736b64881dc31aa57b9
mistermur <brian.j.murillo@gmail.com>
2019-01-22 16:50:30 -0500

    pizza

--- bin/run.rb
+++ bin/run.rb
@@ -6,7 +6,7 @@ require 'pry'
 def get_food_from_api(food)
   #make the web request
   # puts 'in get character'
-  apikey = "https://api.nal.usda.gov/ndb/search/?format=json&q=cookie&sort=n&max=25&offset=0&api_key=m7tJlPDeol0BRpU94StlarX7J2owCr33rxxJS8mP"
+  apikey = "https://api.nal.usda.gov/ndb/search/?format=json&q=#{food}&sort=n&max=25&offset=0&api_key=m7tJlPDeol0BRpU94StlarX7J2owCr33rxxJS8mP"
   response_string = RestClient.get(apikey)
   response_hash = JSON.parse(response_string)
   binding.pry
@@ -16,5 +16,5 @@ def get_food_from_api(food)
   # binding.pry
 end
 
-get_food_from_api('Cheese, cheddar')
+get_food_from_api('pizza')
 puts "HELLO WORLD"
